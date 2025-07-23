ce1a2584ee4c4545297687e37e0f24a92de83ad6
NguyenBach <nguyencongbachhg@gmail.com>
2025-07-23 22:41:31 +0700

    Translate & update code

--- catalog/views.py
+++ catalog/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse
 from catalog.models import Book, Author, BookInstance, Genre
-from catalog.models import LOAN_STATUS  
+from catalog.models import LOAN_STATUS
+from django.views import generic
+
+# Define constants
+PAGINATION_SIZE = 10
+
 def index(request):
     """View function for home page of site."""
-
     # Generate counts of some of the main objects
     num_books = Book.objects.count()
     num_instances = BookInstance.objects.count()
-
+    
     # Available books (status = LOAN_STATUS.available)
     num_instances_available = BookInstance.objects.filter(
         status__exact=LOAN_STATUS['AVAILABLE']
     ).count()
-
+    
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
@@ -22,15 +27,34 @@ def index(request):
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
     }
-
+    
     # Render the HTML template index.html with the data in the context variable
     return render(request, "index.html", context=context)
-  
-from django.views import generic
+
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 10
+    paginate_by = PAGINATION_SIZE
+
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        book = self.get_object()
+
+        # Get the list of genres as a list to join in the template
+        context['genres'] = book.genre.values_list('name', flat=True)
+
+        # Get the list of book instances
+        context['book_copies'] = book.bookinstance_set.all()
+
+        # Create a link to the author's detail page (if available)
+        try:
+            author = book.author
+            context['author_url'] = reverse('author-detail', args=[str(author.id)])
+        except:
+            context['author_url'] = '#'
+
+        return context
